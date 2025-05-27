4fdd90911287bd8ab51b0432408b7200c38a379f
Michael Lai <michaellai@Mac.attlocal.net>
2025-05-27 15:12:13 -0500

    removed cookies for popup

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -12,37 +12,13 @@ function closePopup() {
     if (popup) {
         popup.classList.remove('show');
         document.body.style.overflow = 'auto'; // Restore scrolling
-        
-        // Set a cookie so popup doesn't show again for 24 hours
-        setCookie('popup_shown', 'true', 1);
     }
-}
-
-// Cookie functions
-function setCookie(name, value, days) {
-    const expires = new Date();
-    expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
-    document.cookie = name + '=' + value + ';expires=' + expires.toUTCString() + ';path=/';
-}
-
-function getCookie(name) {
-    const nameEQ = name + "=";
-    const ca = document.cookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
 }
 
 // Show popup when page loads (with delay)
 document.addEventListener('DOMContentLoaded', function() {
-    // Check if popup was already shown today
-    if (!getCookie('popup_shown')) {
         // Show popup after 2 seconds
         setTimeout(showPopup, 2000);
-    }
 });
 
 // Close popup when clicking outside of it
